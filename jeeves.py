16746816510778d6832efb65abd19f7de2503e1c
Anatoly Scherbakov <altaisoft@gmail.com>
2023-10-02 20:51:43 +0400

    Implement `j example` & `j serve`

---
+++ jeeves.py
@@ -0,0 +1,32 @@
+import rich
+from sh import mkdocs
+
+from documented import Documented
+
+
+class Example(Documented):
+    """
+    This is an example Markdown printout.
+
+    * This _message_
+    * is *printed*
+    * from a **Python** `docstring`.
+
+    ## It even supports code
+
+    ```python
+    def something():
+        ...
+    ```
+
+    > Cool indeed!
+    """
+
+
+def example():
+    """Print an example `Documented` object in Markdown."""
+    rich.print(Example())
+
+
+def serve():
+    mkdocs.serve('-a', 'localhost:6854')
